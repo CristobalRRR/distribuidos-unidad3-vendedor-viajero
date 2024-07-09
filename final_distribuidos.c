d86a44673fb0f85889c6d58286278a172169d126
CristobalRRR <83361613+CristobalRRR@users.noreply.github.com>
2024-07-09 02:48:16 -0400

    Sin MPI, comentarios mejorados

--- final_distribuidos.c
+++ final_distribuidos.c
@@ -48,17 +48,15 @@ Cromosoma * poblacion;
 void mostrar_cromosoma(Cromosoma *ptr_cromosoma){
     float fitness_a_invertir = ptr_cromosoma->fitness;
     float distancia = pow(10, 10 / fitness_a_invertir);
-    printf("Costo de del viaje: %f\n", distancia);
+    printf("Costo de viaje: %f\n", distancia);
     printf("Fitness = %f, Genes= ", ptr_cromosoma->fitness);
     for (int i = 0; i < largo_cromosoma; i++)
         printf("%d_", ptr_cromosoma->genes[i]);
     printf("\n");
 }
-//Muestra los valores de los cromosomas para todos los miembros de la poblacion
+//Se adapto para mostrar el valor del cromosoma del primer miembro de la poblacion luego de ordenarla
 void mostrar_poblacion(Cromosoma *poblacion){
-    //for(int i = 0; i < tamaño_poblacion; i++){
-        mostrar_cromosoma(&poblacion[tamaño_poblacion-1]);
-   // }
+        mostrar_cromosoma(&poblacion[0]);
 }
 
 //Adaptacion del metodo para recibir el nombre del archivo por teclado
@@ -86,6 +84,7 @@ void leer_desde_archivo(){
         printf("Error, DIMENSION no encontrada.\n");
         exit(0);
     }
+    //se asigna num_ciudades(cantidad de ciudades) a la variable global largo_cromosoma para utilizarlo en el resto del codigo
     largo_cromosoma = num_ciudades;
         
     //Luego de saber la dimension, escanea las coordenadas
@@ -116,7 +115,10 @@ void mostrar_matriz_distancia(){
             printf("(%d,%d) = %.4f\t",i,j, matriz_distancia[i][j]);
     }
 }
-    
+
+//Esta funcion lee desde el archivo para obtener las ciudades y sus coordenadas,
+//de manera que pueda hacer la matriz distancia aplicando la funcion euclidiana
+//para usar estas distancias mas tarde al calcular el costo de una ruta
 void iniciar_matriz_distancia(){
     leer_desde_archivo();
     matriz_distancia = malloc(sizeof (float *)* largo_cromosoma);
@@ -153,7 +155,7 @@ void calcular_fitness(Cromosoma *ptr_cromosoma){
     ptr_cromosoma-> fitness= fitness;
 }
 
-//Send each chromosome to the above function
+//Esta funcion se encarga de enviar a cada cromosoma de la poblacion para que se le obtenga la aptitud
 void calcular_fitness_poblacion(Cromosoma *poblacion){
     for(int i = 0; i < tamaño_poblacion; i++)
         calcular_fitness(&poblacion[i]);
@@ -167,7 +169,7 @@ int obtenerNumAleatorio(){
 }
 
 
-//funcion que llena los cromosomas y genera las las posibles rutas aleatorias
+//Funcion que llena los cromosomas y genera las las posibles rutas aleatorias
 void llenar_cromosoma_aleatoriamente(Cromosoma *cromo){
     int array[largo_cromosoma];
     cromo->genes = malloc(largo_cromosoma * sizeof (int));
@@ -192,7 +194,7 @@ void intercambiar_cromosomas(Cromosoma *pobla, int src, int dest){
 	pobla[dest] = cromo;
 }
 
-//Ordena la poblacion segun el valor de aptitud de cada cromosoma
+//Ordena la poblacion segun el valor de aptitud de mayor a menor valor de aptitud de cada cromosoma
 void ordena_poblacion(Cromosoma *poblacion){
     for(int i = 0; i < tamaño_poblacion; i++)
         for(int j = i+1; j < tamaño_poblacion; j++)
@@ -200,7 +202,7 @@ void ordena_poblacion(Cromosoma *poblacion){
                 intercambiar_cromosomas(poblacion, i, j);
 }
 
-//Esta funciona genera un indice aleatorio de un cromosoma para ser usado
+//Esta funcion genera un indice aleatorio de un cromosoma para ser usado
 //en el crossover
 int indice_aleatorio_cromosoma(){
     int seed = (unsigned)(time(NULL)+rand());
@@ -211,7 +213,9 @@ int indice_aleatorio_cromosoma(){
 //Aqui se hace la seleccion de los cromosomas a ser usados para el crossover
 void seleccionar(Cromosoma *pobla){
     int n = (40*tamaño_poblacion)/100;
-    //Swapping 10 chromosomes from the last 50% to the middle
+    //Intercambia 10 cromosomas desde la mitad hasta el final de la población
+    //de forma aleatoria para no seleccionar unicamente aquellos de mayor aptitud
+    //y por lo tanto no quedar atrapado en minimos locales
     for(int i = 0; i < (10*tamaño_poblacion)/100; i++){ 
         int randNb =(tamaño_poblacion/2) + indice_aleatorio_cromosoma()%(tamaño_poblacion/2);
         intercambiar_cromosomas(poblacion, n+i, randNb);
@@ -227,7 +231,8 @@ void mostrar_fitness(){
 }
 
 
-//ESta funcion revisa la diferencia entre 2 cromosomas
+//Esta funcion revisa la diferencia de genes entre 2 cromosomas
+//para ver cuanta diversidad existe y ser usado o no en el crossover
 float porcentaje_de_diferencia(Cromosoma cro1, Cromosoma cro2){
 	float sum = 0;
 	for(int i = 0; i < largo_cromosoma; i++)
@@ -236,7 +241,8 @@ float porcentaje_de_diferencia(Cromosoma cro1, Cromosoma cro2){
 	return (sum*100)/largo_cromosoma;
 }
 
-//Revisa si existe un vertice en el cromosoma
+//Revisa si ya existe un vertice (ciudad) en la ruta para
+//evitar repeticiones, porque el TSP exige visitar 1 vez la ciudad
 int existe_vertice(Cromosoma *crom, int x){
 	for(int i=0; i<largo_cromosoma; i++)
 		if(x == crom->genes[i])
@@ -244,25 +250,32 @@ int existe_vertice(Cromosoma *crom, int x){
 	return 0;
 }
 
-//Genera al hijo obtenido del crossover
+//Genera al hijo juntando partes del cromosoma p y m
 void crear_hijoV2(Cromosoma p, Cromosoma m, Cromosoma *Crom){
 	int z=1;
+    
+    //El valor n selecciona un gen aleatorio para empezar a copiar desde un padre, esto mas adelante
 	int n = obtenerNumAleatorio()%(largo_cromosoma);
-
+    
+    //Primero inicializa al gen hijo llamado Crom en 0 para dejarlo listo a recibir genes
 	for(int i=0; i < largo_cromosoma;i++)
 		Crom->genes[i] =0;
-        
+
+    //Aqui se copian genes del padre p a partir de la posicion n
 	for(int i = n; i < n+((largo_cromosoma*30)/100);i++){
 		z=i%largo_cromosoma;
 		Crom->genes[z]=p.genes[z];
 	}
+    //Luego se define una forma de terminar al cromosoma hijo utilizando genes del padre m
+    //a partir del punto que quedaron los genes del padre p, esto lo marca i
 	int c=0;
 	int i=(z+1)%largo_cromosoma;
 	while(i!=z){
 		c = c%largo_cromosoma;
 		if(existe_vertice(Crom, m.genes[c]) != 1)
 			Crom->genes[i] = m.genes[c];
-
+    //Se hacen verificacions del vertice para que los genes de m no se repitan con los que
+    //p ya haya añadido al nuevo cromosoma
 		else{
 			if(Crom->genes[i] == 0){
 				while(existe_vertice(Crom, m.genes[c]) == 1){
@@ -277,13 +290,17 @@ void crear_hijoV2(Cromosoma p, Cromosoma m, Cromosoma *Crom){
 	}
 }
 
-//Esta funcion realiza el crossover entre 2 cromosomas
+//Esta funcion realiza un crossover entre 2 cromosomas
 void crossoverV2(Cromosoma *pobla){
 	int nb=0;
+    //Para el crossover recorre la mitad de la poblacion
 	for(int i = 0; i < (tamaño_poblacion/2) ; i++){
+    //Dentro de este ciclo comprueba a los cromosomas para obtener posibles padres
+    //asegurando diversidad con el porcentaje de diferencia menor a 70%
 		do{
 			nb= obtenerNumAleatorio() % (tamaño_poblacion/2);
 		}while(nb == i && porcentaje_de_diferencia(pobla[i], pobla[nb]) < 70);
+    //Si encuentra padres adecuados crea un cromosoma hijo almacenandolo en la otra mitad de la poblacion
 		crear_hijoV2(pobla[i], pobla[nb], &pobla[(tamaño_poblacion/2) +i]);
 	}
 }
@@ -291,11 +308,15 @@ void crossoverV2(Cromosoma *pobla){
 //Esta funcion realiza las mutaciones del cromosoma reordenando
 //aleatoriamente las rutas en base a un numero aleatorio generado por una semilla
 void mutacion(Cromosoma *pobla){
-//En este ciclo for se define un z<5 que significa que hara 5 mutaciones al cromosoma
+//En este ciclo for se define un z<5 que significa que hara 5 mutaciones
 	for(int z=0; z<5; z++){
+    //i y j seleccionan un gen aleatorio para mutar.
+    //Mientras que k selecciona al cromosoma que recibe la mutacion, esta mutacion afecta
+    //al 80% de la poblacion, excluyendo el 20% inicial, para no mutar solo los mas aptos y tener mas variedad
 		int i = obtenerNumAleatorio()%(largo_cromosoma);
 		int j = obtenerNumAleatorio()%(largo_cromosoma);
 		int k = obtenerNumAleatorio()%(tamaño_poblacion -(20*tamaño_poblacion/100));
+    //Luego de escoger intercambia los genes de la posicion i y j entre ellos para formar la mutacion
 		int temp = pobla[(20*tamaño_poblacion/100)+k].genes[j];
 		pobla[(20*tamaño_poblacion/100)+k].genes[j] = pobla[(20*tamaño_poblacion/100)+k].genes[i];
 		pobla[(20*tamaño_poblacion/100)+k].genes[i] = temp;
@@ -303,9 +324,10 @@ void mutacion(Cromosoma *pobla){
 }
 
 int main(int argc, char **argv){
-    tamaño_poblacion = 50; // Numero de cromosomas
+    tamaño_poblacion = 50; // Numero de cromoksomas
     numero_iteraciones = 1000; //Numero de iteraciones o generaciones
     
+
     //Estas lineas piden ingresar el nombre del archivo para obtener las coordenadas
     printf("Ingrese el nombre completo del archivo TSP\n");
     scanf("%s",nombre_archivo); 
@@ -314,33 +336,41 @@ int main(int argc, char **argv){
     //Se inicia el calculo de la distancia euclidiana entre todas las ciudades
     //para que sea mas facil calcular los costos más tarde
     iniciar_matriz_distancia(); 
-    //Se asigna memoria dinamica a la poblacion de rutas (cromosomas) con un largo igual a la dimension(numero de ciudades)
+
+    //Se asigna memoria dinamica a la poblacion de rutas (cromosomas) para acomodar los valores
     poblacion = (Cromosoma *)malloc(tamaño_poblacion*sizeof(Cromosoma));
     
-
     //Iniciamos el reloj para medir cuanto se demora
     clock_t tiempo;
     tiempo = clock();
+
     //Se inicia la creacion de cromosomas
     for(int i = 0; i < tamaño_poblacion; i++){
         llenar_cromosoma_aleatoriamente(&poblacion[i]);
     }
-    //Se llama a la funcion que ordena la poblacion de cromosomas en base al nivel de fitness(aptitud)
+    //Se llama a la funcion que ordena la poblacion de cromosomas en base al valor de fitness(aptitud)
     ordena_poblacion(poblacion);
     int  i = 0;
     while(i < numero_iteraciones){
-        seleccionar(poblacion); //Select 50% of poblacion
-        crossoverV2(poblacion); //Perform the crossover
-        mutacion(poblacion); //Perform mutation
-        calcular_fitness_poblacion(poblacion); // Calculate new fitness value
-        ordena_poblacion(poblacion); //Sort acc to fitness value
+        seleccionar(poblacion); //Seleccionar la mitad de la poblacion
+        crossoverV2(poblacion); //Realiza el crossover
+        mutacion(poblacion); //Hace el proceso de mutacion
+        calcular_fitness_poblacion(poblacion); //Calcula un valor de fitness para la poblacion
+        ordena_poblacion(poblacion); //Ordena la poblacion segun la aptitud
         i++;
     }
 
     //Tomamos tiempo actual del reloj y lo restamos al inicial para obtener el tiempo
     tiempo = clock() - tiempo;
     double tiempo_de_ejecucion = ((double)tiempo)/CLOCKS_PER_SEC;
+
+    //Muestra el tiempo de ejecucion del problema del vendedor viajero
     printf("Tiempo: \t %f \n", tiempo_de_ejecucion);
+
+    //Llama a la funcion para mostrar el cromosoma con menor costo del viaje(distancia),
+    //fitness(mejor valor de aptitud) y el mejor orden de genes(ruta de ciudades)
     mostrar_poblacion(poblacion);
-    return 0;
+
+
+    return 0;//finalizacion del programa principal
 }
